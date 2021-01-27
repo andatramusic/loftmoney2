9155dcfa1afeb1f392408f4dbbc2ec4813542df9
andatramusic <55131181+andatramusic@users.noreply.github.com>
2021-01-27 19:08:53 +0500

    Initial commit

---
+++ app/src/main/java/com/andatra/loftmoney/MainActivity.java
@@ -0,0 +1,25 @@
+package com.andatra.loftmoney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnClick).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newActivity = new Intent(getApplicationContext(), SecondActivity.class);
+                newActivity.putExtra("COST_ID",1);
+                startActivity(newActivity);
+            }
+        });
+    }
+}
